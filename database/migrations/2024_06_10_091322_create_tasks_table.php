96e16f82c753f813ea73bd1fd6f7952073c99563
kokhlikyan <koxlikyan1995@gmail.com>
2024-06-10 13:26:04 +0400

    feat: create models and migrations

---
+++ database/migrations/2024_06_10_091322_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->integer('block_id')->nullable();
+            $table->integer('goal_id')->nullable()->comment('can choose one of these goals or habits');
+            $table->integer('habit_id')->nullable()->comment('can choose one of these goals or habits');
+            $table->string('title');
+            $table->text('details')->nullable();
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
---
+++ database/migrations/2024_06_10_091652_create_recurrences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\RepeatTypes;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recurrences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->enum('type', array_column(RepeatTypes::cases(), 'value'));
+            $table->integer('interval')->default(1);
+            $table->integer('day_of_week')->nullable();
+            $table->integer('day_of_month')->nullable();
+            $table->integer('month_of_year')->nullable();
+            $table->string('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recurrences');
+    }
+};
