b8aed704730ded3047d3189b200b31c961ffbe6a
kokhlikyan <koxlikyan1995@gmail.com>
2024-07-02 16:38:28 +0400

    fix: response error

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Settings;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        $settings = Settings::query()->where('user_id', $user->id)->first();
+        if (!$settings) {
+            Settings::query()->create([
+                'user_id' => $user->id
+            ]);
+        }
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
