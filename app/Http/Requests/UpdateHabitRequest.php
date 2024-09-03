e5b14d4256e61c5d97000244b681a35527a70120
kokhlikyan <koxlikyan1995@gmail.com>
2024-09-03 15:27:18 +0400

    fix: detail validation

--- app/Http/Requests/UpdateHabitRequest.php
+++ app/Http/Requests/UpdateHabitRequest.php
@@ -24,7 +24,7 @@ class UpdateHabitRequest extends FormRequest
     {
         return [
             'title' => ['string', 'max:255'],
-            'details' => ['string'],
+            'details' => ['nullable', 'string'],
         ];
     }
 }
